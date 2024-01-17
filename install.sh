9b961ab8d92d736827298b03f37e0865e862446d
Tyler C <18288340+TylerCode@users.noreply.github.com>
2024-01-17 11:38:47 -0700

    Better comments on install script

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ fi
 cp "${BUILD_DIR}/main" "${BIN_DIR}/pi-summoner"
 chmod +x "${BIN_DIR}/pi-summoner"
 
-# Step 2: Copy the service file and set permissions
+# Copy the service file and set permissions
 cp "${SERVICE_FILE_DIR}/pi-summoner.service" "${SERVICE_DIR}"
 chmod 644 "${SERVICE_DIR}/pi-summoner.service"
 
-# Step 3: Reload systemd and enable/start the service
+# Reload systemd and enable/start the service
 systemctl daemon-reload
 systemctl enable pi-summoner.service
 systemctl start pi-summoner.service
