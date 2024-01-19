9d3017ef8fbe2786fc04244a64ad8d25601f810b
Tyler C <18288340+TylerCode@users.noreply.github.com>
2024-01-18 19:35:28 -0700

    Updated build script for non-pi builds

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 
 echo "Build started."
 
-gcc main.c -o main -lpigpio -lmicrohttpd -lrt
+# Check for an environment variable to determine if we are compiling for a non-Raspberry Pi environment
+if [ "$NON_RPI_BUILD" == "1" ]; then
+    echo "Compiling for a non-Raspberry Pi environment..."
+    gcc main.c -o main -lmicrohttpd -lrt
+else
+    echo "Compiling for Raspberry Pi..."
+    gcc main.c -o main -lpigpio -lmicrohttpd -lrt
+fi
+
 if [ $? -ne 0 ]; then
     echo "Compilation failed."
     exit 1
