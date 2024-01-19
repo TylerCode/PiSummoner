29098541a3526286a9d5d6005e931aa98e6e7322
Tyler C <18288340+TylerCode@users.noreply.github.com>
2024-01-18 19:20:54 -0700

    Added some logging to the build script.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@
 echo "Build started."
 
 gcc main.c -o main -lpigpio -lmicrohttpd -lrt
+if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
 chmod +x main
-rm -r build
+
+rm -rf build
 mkdir build
 mv main build
 
